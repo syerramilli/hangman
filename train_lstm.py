992d28f0877f6cbad6ea086ca0c4e5b7f1b7f989
Suraj Yerramilli <surajyerramilli@gmail.com>
2024-04-02 11:04:32 -0500

    adding device option and saving training losses

--- train_lstm.py
+++ train_lstm.py
@@ -33,29 +33,49 @@ if __name__ == '__main__':
     dataset = MaskedLettersDataset(dictionary)
     dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
 
+    # check if GPU is available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     # instantiate the model
     print('Instantiating the model')
     # the vocab size is 28 because we have 26 letters and 2 special characters
     # 0 for padding and 27 for masked characters
-    model = CharBiLSTM(28, EMBEDDING_DIM, HIDDEN_DIM)
+    model = CharBiLSTM(28, EMBEDDING_DIM, HIDDEN_DIM).to(device)
     
     # define the loss function and the optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+
+    # training losses
+    training_losses = []
 
     print('Training the model')
     for epoch in range(EPOCHS):
         with tqdm(dataloader, desc=f'Epoch {epoch}', unit='batch') as t:
             for i, (masked_words, label_words, mask_positions) in enumerate(t):
                 optimizer.zero_grad()
+                # Transfer data to device
+                masked_words = masked_words.to(device)
+                label_words = label_words.to(device)
+                mask_positions = mask_positions.to(device)
+                
+                # Forward pass
                 logits = model(masked_words)
                 loss = criterion(logits[mask_positions == 1], label_words[mask_positions == 1])
+
+                # Backward pass
                 loss.backward()
                 optimizer.step()
                 #if i % 100 == 0:
                 t.set_postfix({'Loss': loss.item()})
                 t.update(1)
+            
+            training_losses.append(loss.item())
 
     # save the model
     print('Saving the model')
-    torch.save(model.state_dict(), config['saving']['filename'])
+    torch.save(model.state_dict(), config['saving']['model_state'])
+
+    with open(config['saving']['training_loss_path'], 'w') as f:
+        for loss in training_losses:
+            f.write(f'{loss}\n')
